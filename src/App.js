8cf70754ab8015229e02e8340d4be58f13360c01
Asmar <asmarcd@gmail.com>
2020-12-11 11:06:35 -0800

    material ui practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import TextField from '@material-ui/core/TextField'
+
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import {orange, green} from '@material-ui/core/colors'
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #333, #999)',
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: 'white',
+    padding: '0 30px'
+  }
+})
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[400],
+    },
+    secondary: {
+      main: green[400],
+    }
+  }
+})
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>
+}
+
+function CheckboxExample() {
+
+  const [checked, setChecked] = React.useState(true)
+
+  return (
+    <FormControlLabel
+      control={<Checkbox
+        checked={checked}
+        icon={<DeleteIcon />}
+        checkedIcon={<SaveIcon />}
+        onChange={(e) => setChecked(e.target.checked)}
+        inputProps={{
+          'aria-label': 'secondary checkbox'
+        }}
+      />}
+      label='testing checkbox'
+    />
+
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <ButtonStyled />
+          <TextField
+            variant="filled"
+            color="secondary"
+            type="email"
+            label="the time"
+            placeholder="test@test.com"
+          />
+          <CheckboxExample />
+          <ButtonGroup size="large" variant="contained" color="primary">
+            <Button startIcon={<SaveIcon />}>
+              Save
+        </Button>
+            <Button startIcon={<DeleteIcon />}>
+              Discard
+        </Button>
+          </ButtonGroup>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
